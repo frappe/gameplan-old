69a6d9353165deb6632cc1c426d6b5ab57b2cc4c
Valmik Jangla <valmik@erpnext.com>
2016-01-20 16:04:40 +0530

    Fixed username issue Fixed issue where comment didn't appear automatically Moved discussion text input

--- gameplan/game_plan/doctype/discussion_user/discussion_user.py
+++ gameplan/game_plan/doctype/discussion_user/discussion_user.py
@@ -9,24 +9,28 @@ from frappe.website.website_generator import WebsiteGenerator
 
 color_options = ['#1FBBA0', "#D40222", "#77D24B"]
 
+
 class DiscussionUser(WebsiteGenerator):
-	website = frappe._dict(
-		template = "templates/generators/user.html",
-		condition_field = "published",
-		page_title_field = "username",
-		no_cache = True
-	)
+    website = frappe._dict(
+        template="templates/generators/user.html",
+        condition_field="published",
+        page_title_field="username",
+        no_cache=True
+    )
 
-	def validate(self):
-		super(DiscussionUser, self).validate()
-		self.parent_website_route = ''
-		self.page_name = self.name
+    def autoname(self):
+        pass
 
-		if not self.color:
-			self.color = color_options[random.randrange(0,3)]
+    def validate(self):
+        super(DiscussionUser, self).validate()
+        self.parent_website_route = ''
+        self.page_name = self.name
 
-	def get_context(self, context):
-		context.discussions = frappe.get_all("Discussion",
-			fields=["page_name", "parent_website_route", "title"],
-			filters={"published": 1, "owner": frappe.session.user},
-			order_by="creation desc", limit_page_length=20)
+        if not self.color:
+            self.color = color_options[random.randrange(0, 3)]
+
+    def get_context(self, context):
+        context.discussions = frappe.get_all("Discussion",
+                                             fields=["page_name", "parent_website_route", "title"],
+                                             filters={"published": 1, "owner": frappe.session.user},
+                                             order_by="creation desc", limit_page_length=20)
